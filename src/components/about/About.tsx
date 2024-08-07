2420ad317f394b2fd68ede799c7f82dbe7c6157e
Mandip Tamang <136143099+mandiptmg@users.noreply.github.com>
2024-08-07 16:09:08 +0545

    Update About.tsx

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -67,17 +67,16 @@ const About = () => {
               </tbody>
             </table>
           </div>
-            <Link
-      href='/mandiptamang.pdf'
-
-    
-                 <a download="resume.pdf"   className='px-5 py-2 font-medium bg-[#1F618D] text-white w-fit transition-all shadow-[3px_3px_0px_black] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[3px] flex items-center gap-1'
-    > 
-      <FaDownload className='text-xl sm:text-2xl ' />{' '}
-      <span className='text-base md:text-lg font-semibold'>
-        Resume
-      </span>
-                   </a>
+           <Link href="/mandiptamang.pdf" passHref>
+      <a
+        download="resume.pdf"
+        className='px-5 py-2 font-medium bg-[#1F618D] text-white w-fit transition-all shadow-[3px_3px_0px_black] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[3px] flex items-center gap-1'
+      >
+        <FaDownload className='text-xl sm:text-2xl' />
+        <span className='text-base md:text-lg font-semibold'>
+          Resume
+        </span>
+      </a>
     </Link>
         </div>
       </div>
