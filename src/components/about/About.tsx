19d6d94dd1478cdcc40f70959fec11bf6145f2c5
Mandip Tamang <136143099+mandiptmg@users.noreply.github.com>
2024-08-07 15:51:56 +0545

    resume download

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -68,7 +68,7 @@ const About = () => {
             </table>
           </div>
           <a
-            href={'https://drive.google.com/file/d/1jNkQdODtnHKo-ffPyh9HHs5y152IoYpb/view'}
+            href={pdf}
             download='mandiptamang.pdf'
             className='px-5 py-2 font-medium bg-[#1F618D] text-white w-fit transition-all shadow-[3px_3px_0px_black] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[3px] flex items-center gap-1'
           >
