4db42dc69346cae52a2669d521d0c6c938dcb0c8
Mandip Tamang <136143099+mandiptmg@users.noreply.github.com>
2025-01-07 23:07:44 +0545

    Delete src directory

--- src/app/Provider.tsx
+++
@@ -1,8 +0,0 @@
-import { AppProvider } from '@/context/context'
-export const Provider = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div >
-      <AppProvider>{children}</AppProvider>
-    </div>
-  )
-}
